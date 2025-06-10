5d696d6ec338bc643d04920d9c6762b3be696c44
Jemis Solanki <147297581+jemissolanki@users.noreply.github.com>
2025-06-10 16:52:48 +0530

    middleware code fixes

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -5,7 +5,7 @@ const protect = async (req, res, next) => {
   let token;
   if (
     req.headers.authorization &&
-    req.headers.authorization.startsWith("bearer")
+    req.headers.authorization.startsWith("Bearer")
   ) {
     try {
       token = req.headers.authorization.split(" ")[1];
